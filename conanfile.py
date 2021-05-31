a966be7082b0da2d14034bdbebd0aad75118b679
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2021-05-31 11:20:58 +0100

    update to 4.6.0-rc3

--- conanfile.py
+++ conanfile.py
@@ -19,34 +19,33 @@ class ConanLexFloatServer(ConanFile):
     @property
     def _package_contents_dir(self):
         if self.settings.os == 'Linux':
-            la_arch = {
-                'x86': 'i386',
+            return {
                 'x86_64': 'amd64',
                 'armv8': 'arm64',
                 'armv8.3': 'arm64',
             }[str(self.settings.arch)]
-            return os.path.join('gcc', la_arch)
 
         if self.settings.os == 'Windows':
-            la_arch = {
-                'x86': 'x86',
-                'x86_64': 'x64',
-            }[str(self.settings.arch)]
-            return os.path.join('vc16', la_arch)
+            return 'x64'
 
         if self.settings.os == 'Macos':
-            return os.path.join('universal')
+            return {
+                'x86_64': 'x86_64',
+                'armv8': 'arm64',
+                'armv8.3': 'arm64',
+            }[str(self.settings.arch)]
         raise ConanInvalidConfiguration('Libraries for this configuration are not available')
 
     def package(self):
         if self.settings.os != 'Windows':
             # Make executable
-            server_exe = os.path.join(self._package_contents_dir, 'LexFloatServer')
+            server_exe = os.path.join(self._package_contents_dir, 'lexfloatserver')
             st = os.stat(server_exe)
             os.chmod(server_exe, st.st_mode | stat.S_IEXEC)
-            self.copy("LexFloatServer", dst="bin", src=self._package_contents_dir)
+            self.copy("lexfloatserver", dst="bin", src=self._package_contents_dir)
         else:
             self.copy("LexFloatServer.exe", dst="bin", src=self._package_contents_dir)
+        self.copy("config.yml", dst="share/lexfloatserver/sample", src=self._package_contents_dir)
 
     def package_info(self):
         self.env_info.path.append(os.path.join(self.package_folder, "bin"))
